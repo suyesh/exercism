d589396daba992d0d5c8f04993ba32179c21406b
Suyesh Bhandari <suyeshb@gmail.com>
2016-11-08 00:42:14 -0500

    refactor

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,7 +1,7 @@
 class Pangram
   class << self
-    def is_pangram?(str)
-      str.downcase.chars.uniq.sort.join.include?(('a'..'z').to_a.join)
+    def is_pangram?(string)
+      string.downcase.chars.uniq.sort.join.include?(('a'..'z').to_a.join)
     end
   end
 end
